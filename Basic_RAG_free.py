c98f74f93eae80aa6c707309d68ccb30daeebe07
lerenardrouxx <andrin.kohler@stud.hslu.ch>
2025-10-15 07:22:11 +0200

    added prompt structure and citing sources in output

--- Basic_RAG_free.py
+++ Basic_RAG_free.py
@@ -33,12 +33,7 @@ from langchain_ollama import ChatOllama
 embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
 
-###we could also test other possible embeddings and compare performance e.g "nomic-embed-text" from ollama
 
-#from langchain_ollama import OllamaEmbeddings
-
-#embeddings = OllamaEmbeddings(model="nomic-embed-text")
-#vectorstore = FAISS.from_documents(texts, embeddings)
 
 #mechanism to rebuild the vectorstore if needed in case something was changed with force_rebuild = True
 force_rebuild = True
@@ -54,7 +49,7 @@ if os.path.exists(faiss_path):
 else:
     loader = PyPDFLoader("George_Orwell_1984.pdf")
     documents = loader.load()
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=2000, chunk_overlap=400)
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=700, chunk_overlap=150)
     texts = text_splitter.split_documents(documents)
     vectorstore = FAISS.from_documents(texts, embeddings)
     vectorstore.save_local(faiss_path)
@@ -62,11 +57,8 @@ else:
 # Use local Llama 3.1 model via Ollama
 llm = ChatOllama(model="llama3.1:8b", temperature=0.1)
 
-## we could also test other chain_types such as "map_reduce" (sends each junk separately to the llm then summarizes results)
-## or "refine" (sends each junk individually to the llm)
 
-#we could also play around with the numbers of chunks retrieved to see how the result is influenced:
-# e.g retriever = vectorstore.as_retriever(search_kwargs={"k": 6}
+
 
 ###Prompt template
 from langchain.prompts import PromptTemplate
@@ -76,7 +68,8 @@ custom_prompt = PromptTemplate(
     template=(
         "You are a careful assistant. Use ONLY the context to answer.\n"
         'If the answer is not fully supported by the context, say: "I do not know based on the provided context."'
-        "Always cite sources as [page X] after each claim.\n\n"
+        "Always copy the exact page tags like [page 136] from the context when citing."
+        "Do not invent new tags.\n\n"
         "Answer the question based only on the following context:\n"
         "{context}\n"
         "---\n"
@@ -87,13 +80,19 @@ custom_prompt = PromptTemplate(
 qa_chain = RetrievalQA.from_chain_type(
     llm=llm,
     chain_type="stuff",
-    retriever=vectorstore.as_retriever(),
+    #add mmr (to check for similarity in chunks)
+    retriever=vectorstore.as_retriever(
+        search_type="mmr",
+        search_kwargs={"k":5, "fetch_k":40, "lambda_mult": 0.8} #lambda mult is a weight to define how important relevance is to diversity
+    ),
     return_source_documents = True,
     chain_type_kwargs={"prompt": custom_prompt}
 )
 
-query = "When does Winston see Julia for the first time?"
+query = "What is the Junior Anti-Sex League"
 
+#"When does Winston see Julia for the first time?"
+#What is the Junior Anti-Sex League Orwell is writing about?
 #Print the prompt
 
 docs = qa_chain.retriever.get_relevant_documents(query)
